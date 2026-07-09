bc862d47b26220f0fa167cac2d18ece9f8f7af06
unknown <jerryjeriomio19@gmail.com>
2026-07-09 15:15:29 +0600

    initial project setup

---
+++ src/main/java/com/example/demo/SocialMediaAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaAgentApplication.class, args);
+	}
+
+}
