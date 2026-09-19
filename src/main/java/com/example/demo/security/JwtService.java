f9f648286f02f3c4a87d17a8164a8af192fbaae5
unknown <jerryjeriomio19@gmail.com>
2026-09-19 18:58:05 +0600

    Add JWT authentication filter

--- src/main/java/com/example/demo/security/JwtService.java
+++ src/main/java/com/example/demo/security/JwtService.java
@@ -27,4 +27,14 @@ public class JwtService {
                 .signWith(key)
                 .compact();
     }
+
+    public String extractEmail(String token) {
+
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
 }
